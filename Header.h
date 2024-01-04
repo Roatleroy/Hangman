b3318503c5b8403b2e863c6fc4b4d9a72872935d
Roatleroy <ashtoncrane668@gmail.com>
2024-01-04 15:02:02 -0600

    Creation

---
+++ Header.h
@@ -0,0 +1,18 @@
+#include "iostream"
+#include <vector>
+#include <string>
+#include "iomanip"
+#include <map>
+using namespace std;
+
+class Hangman {
+public:
+
+	string dictionary();
+	void displayMenu();
+	void GenerateWord();
+
+private:
+
+
+};
